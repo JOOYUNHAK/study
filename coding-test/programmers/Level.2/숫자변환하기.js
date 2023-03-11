788bc5dcffc654ad8003df8077261d7a28a3f2da
JOOYUNHAK <wndbsgkr@naver.com>
2023-03-11 22:49:07 +0900

    [coding-test] 프로그래머스 Lv.2 숫자변환하기 문제 추가

---
+++ coding-test/programmers/Level.2/숫자변환하기.js
@@ -0,0 +1,21 @@
+function solution(x, y, n) {
+    var answer = 0;
+
+    if( x == y ) return 0;
+
+    const dp = Array.from({ length: y + 1}, () => Infinity ); 
+    dp[x] = 0;
+
+    for( let i = x; i <= y; i++ ) {
+        if( i + n <= y ) {
+            dp[i+n] = Math.min(dp[i] + 1, dp[i+n]);
+        }
+        if( i * 2 <= y ) {
+            dp[i*2] = Math.min(dp[i] + 1, dp[i*2]);
+        }
+        if( i * 3 <= y ) {
+            dp[i*3] = Math.min(dp[i] + 1, dp[i*3]);
+        }
+    }
+    return dp[y] === Infinity ? -1 : dp[y];
+}
